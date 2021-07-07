786b8125459de816dc7a65af45802cfcc2225b92
Francy <155022F@uom.lk>
2021-07-07 12:27:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./gallery.css";
+import Data from "./contact_card.json";
+import State, { useState } from "react";
 
 function App() {
+  const [allData, setAllData] = useState([]);
+  const [filteredData, setFilteredData] = useState(allData);
+
+  const handleSearch = (event) => {
+    let value = event.target.value.toLowerCase();
+    let result = [];
+    console.log(value);
+    result = allData.filter((data) => {
+      return data.title.search(value) != -1;
+    });
+    setFilteredData(result);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search People..."
+          onChange={(event) => handleSearch(event)}
+        />
+      </div>
+
+      {filteredData.map((post) => {
+        return <p>{post.NAME}</p>;
+        return <p>{post.LOCATION}</p>;
+
+      })}
+
+      <div className="post">
+        {Data.map((post) => {
+          return (
+            <div className="card">
+              <div className="card_header">
+                <div className="title">{post.NAME}</div>
+                <div className="card_image">
+                  <img src={post.PHOTO} />
+                </div>
+                <i class="fa fa-map" aria-hidden="true"></i>
+                {post.LOCATION}
+                <br></br>
+                <i class="fa fa-cake" aria-hidden="true"></i>
+                {post.DOB}
+              </div>
+              <div className="card_body">
+                <p>{post.DESCRIPTION}</p>
+              </div>
+              <div className="card_footer">
+                {post.PHONE}
+
+                <br></br>
+                {post.EMAIL}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
